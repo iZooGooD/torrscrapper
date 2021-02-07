8ba70da9be4b6a6331f1822f70152cfb5bbc8c1f
izoogood <lakshmanchoudhary020@gmail.com>
2021-02-07 11:48:10 +0530

    games,contact form,indexing

--- torrscrapper/urls.py
+++ torrscrapper/urls.py
@@ -5,8 +5,11 @@ urlpatterns=[
     path("",views.index,name="index"),
     path("searchTorrents",views.searchTorrents,name="searchTorrents"),
     path("dmca",views.dmca,name="dmca"),
+    path("privacy_policy",views.privacy_policy,name="privacy_policy"),
+    path("contact_us",views.contact_us,name="contact_us"),
+    path("contact_us/submitform",views.contact_form_submit,name="contact_form_submit"),
     path("categories/movies",views.movies,name="movies"),
     path("categories/movies/<int:movie_id>",views.movies_single,name="movies_single"),
     path("categories/games",views.games,name="games"),
-    path("categories/games/game",views.games_single,name="games_single"),
+    path("categories/games/<int:game_id>",views.games_single,name="games_single"),
 ]
--- torrscrapper/views.py
+++ torrscrapper/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages,auth
 from django.http import HttpResponse
-from torrscrapper.models import Movies
+from torrscrapper.models import Movies,Games,Contact
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from torrscrapper.searchtorrents import client_query
 
@@ -23,6 +23,41 @@ def searchTorrents(request):
 def dmca(request):
     return render(request,"torrscrapper/DMCA.html")
 
+def privacy_policy(request):
+    return render(request,"torrscrapper/privacy.html")
+
+def contact_us(request):
+    context={}
+    context['submission']=False
+    context['errors']=[]
+    return render(request,"torrscrapper/contact.html",context)
+
+def contact_form_submit(request):
+    context={}
+    errors=[]
+    context['success']=False
+    name=request.POST['name']
+    email=request.POST['email']
+    subject=request.POST['subject']
+    message=request.POST['message']
+    if len(name)>100 or len(name)<2:
+        errors.append("Your name should be between 2 to 100 characters")
+    if len(subject)>200 or len(subject)<4:
+        errors.append("Your subject should be between 4 to 200 characters")
+    if len(message)>500 or len(message)<10:
+        errors.append("Message should be between 10 to 500 characters")
+
+    if len(errors)==0:
+        entry = Contact.objects.create(name=name,email=email,subject=subject,message=message)
+        context['success']=True
+        return render(request,"torrscrapper/contact.html",context)
+    else:
+        context['errors']=errors
+        context['success']=False
+        return render(request,"torrscrapper/contact.html",context)
+
+    
+
 def movies(request):
     context={}
     context['search_flag']=False
@@ -39,6 +74,7 @@ def movies(request):
         return render(request,"torrscrapper/category/movies.html",context)
 
     all_entries = Movies.objects.all()
+    context['all_movies_length']=len(all_entries)
     paginator=Paginator(all_entries,20)
     page=request.GET.get('page')
     paged_listing=paginator.get_page(page)
@@ -51,7 +87,29 @@ def movies_single(request,movie_id):
     return render(request,"torrscrapper/category/movies_single.html",context)
 
 def games(request):
-    return render(request,"torrscrapper/category/games.html")
+    context={}
+    context['search_flag']=False
+    if 'keywords' in request.GET:
+        keywords=request.GET['keywords']
+        context['search_flag']=True
+        context['search_keywords']=keywords
+        all_entries = Games.objects.all().filter(title__contains=keywords)
+        context['search_length']=len(all_entries)
+        paginator=Paginator(all_entries,6)
+        page=request.GET.get('page')
+        paged_listing=paginator.get_page(page)
+        context["all_games"]=paged_listing
+        return render(request,"torrscrapper/category/games.html",context)
 
-def games_single(request):
-    return render(request,"torrscrapper/category/games_single.html")
+    all_entries = Games.objects.all()
+    context['all_games_length']=len(all_entries)
+    paginator=Paginator(all_entries,6)
+    page=request.GET.get('page')
+    paged_listing=paginator.get_page(page)
+    context["all_games"]=paged_listing
+    return render(request,"torrscrapper/category/games.html",context)
+
+def games_single(request,game_id):
+    game=get_object_or_404(Games,pk=game_id)
+    context={'game':game}
+    return render(request,"torrscrapper/category/games_single.html",context)

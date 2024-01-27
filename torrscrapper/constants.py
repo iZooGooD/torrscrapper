08934de59372d8bd1568f9f9a526065164f7c2f5
iZooGooD <lakshmanchoudhary020@gmail.com>
2024-01-27 16:51:55 +0530

    Update url

--- torrscrapper/constants.py
+++ torrscrapper/constants.py
@@ -1,3 +1,3 @@
 class SiteURLs:
-    PIRATE_BAY_BASE_URL = "https://pirate-proxy.ing/newapi/q.php?"
+    PIRATE_BAY_BASE_URL = "https://pirate-proxy.date/newapi/q.php?"
     X1337_BASE_URL = "https://1337x.unblockit.ing"
